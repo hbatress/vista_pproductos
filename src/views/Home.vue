4ce9b7d2d3c83adde253d36d44107a9006a812fa
Herber Batres <hbatress@miumg.edu.gt>
2024-12-21 12:00:21 -0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+// filepath: /H:/PROYECTOS/vista trabajo/vistas/src/views/Home.vue
+<template>
+  <div class="home">
+    <h1>Vista Principal</h1>
+    <p>Bienvenido a la vista principal.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
