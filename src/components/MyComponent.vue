2492e56dde1f84720dfd4fb7adcef4b33c24c435
Herber Batres <hbatress@miumg.edu.gt>
2024-12-21 12:10:42 -0600

    se primera configuracion

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,19 @@
+
+// filepath: /H:/PROYECTOS/vista trabajo/vistas/src/components/MyComponent.vue
+<template>
+  <div class="my-component">
+    <p>Este es mi componente.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent'
+}
+</script>
+
+<style scoped>
+.my-component {
+  color: blue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
-// filepath: /H:/PROYECTOS/vista trabajo/vistas/src/views/Home.vue
+// filepath: /H:/PROYECTOS/vista trabajo/vistas/src/views/HomeView.vue
 <template>
   <div class="home">
     <h1>Vista Principal</h1>
     <p>Bienvenido a la vista principal.</p>
+    <MyComponent />
   </div>
 </template>
 
 <script>
+import MyComponent from '../components/MyComponent.vue';
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    MyComponent
+  }
 }
 </script>
 
